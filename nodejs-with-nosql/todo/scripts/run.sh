233c0f89531e2147d4470f15649fae2b870c71d9
Michael Nelson <mnelson@bitnami.com>
2018-07-04 01:16:43 +0000

    Add wait for successful db ping before running app

--- nodejs-with-nosql/todo/scripts/run.sh
+++ nodejs-with-nosql/todo/scripts/run.sh
@@ -8,5 +8,13 @@ readonly installdir=/opt/app
 # The user that should run the app
 readonly system_user=bitnami
 
+# Wait for the database to be available
+while ! mongo "mongodb://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME?${DATABASE_CONNECTION_OPTIONS:-}" --eval '{ping: 1}' --quiet;
+do
+    echo "Waiting for database to become available"
+    sleep 2
+done
+echo "Database available, continuing to run application."
+
 # Typically this is used to start something on foreground
 exec su "${system_user}" -c "cd ${installdir} && npm start"
